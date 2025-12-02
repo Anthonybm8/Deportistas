dee3cf87658b798e1ce90f3709c1525fd4cad85d
Anthonybm8 <anthony.monta2291@utc.edu.ec>
2025-12-01 21:45:17 -0500

    se modificaron lass rutas

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,13 @@ use App\Http\Controllers\PaisController;
 use App\Http\Controllers\DisciplinaController;
 use App\Http\Controllers\DeportistaController;
 
-// RUTAS PÚBLICAS (sin login)
+
 Route::get('/', [AuthController::class, 'showLogin'])->name('login');
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.post');
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
-// RUTAS PROTEGIDAS (requieren login)
-// Los controladores ya tienen la protección en el constructor
+
 Route::resource('pais', PaisController::class);
 Route::resource('disciplina', DisciplinaController::class);
 Route::resource('deportistas', DeportistaController::class);
